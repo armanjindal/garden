d96ef5b6f7df16610efc2a5fba221993b0c48473
Arman Jindal <armanjindal@Armans-MacBook-Air.local>
2023-10-06 13:12:38 +0200

    fix baseUrl

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Garden",
+    pageTitle: "🪴 Digital Garden",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
